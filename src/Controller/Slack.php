c4353b4a242375876715b4fda32116a04b6bb9a1
jRimbault <jacques.rimbault@gmail.com>
2018-03-12 18:21:00 +0100

    fix array operations

--- src/Controller/Slack.php
+++ src/Controller/Slack.php
@@ -17,62 +17,61 @@ class Slack extends Controller
     public function emojis(Request $request)
     {
         $config = Config::Instance()->getArray();
-        $return = '401';
 
-        if ($request->post()->get('token') === $config['verificationtoken']) {
-            Json::writeToFile($_POST, new Path('/var/cache/postdump.json'));
-            $request->post()->do('https://slack.com/api/chat.postMessage', [
-                'token' => $config['token'],
-                'channel' => $request->post()->get('channel_id'),
-                'text' => $this->slackMessage()
-            ]);
-            $return = '';
+        if ($request->post()->get('token') !== $config['verificationtoken']) {
+            http_response_code(401);
+            return '401';
         }
 
-        return $return;
+        Json::writeToFile($_POST, new Path('/var/cache/postdump.json'));
+        $request->post()->do('https://slack.com/api/chat.postMessage', [
+            'token' => $config['token'],
+            'channel' => $request->post()->get('channel_id'),
+            'text' => $this->slackMessage()
+        ]);
+
+        return '';
     }
 
     public function emojishtml(Request $request)
     {
-        $data = Json::DecodeFile(new Path($this->statsFile));
-        $data = $this->sortStructure($data);
-
         return $this->render('slack/statistics/emoji.html.twig', [
-            'emojis' => $data,
+            'emojis' => $this->sortStructure(
+                Json::DecodeFile(new Path($this->statsFile))
+            ),
             'date' => filemtime(new Path($this->statsFile)),
         ]);
     }
 
     public function emojislist(Request $request)
     {
-        $data = Json::DecodeFile(new Path($this->statsFile));
-        // $data = $this->sortStructure($data);
-
         return $this->render('slack/list/emoji.html.twig', [
-            'emojis' => array_reverse($data),
+            'emojis' => array_reverse(
+                Json::DecodeFile(new Path($this->statsFile))
+            ),
             'date' => filemtime(new Path($this->statsFile)),
         ]);
     }
 
-    private function getNfirst($n = 10)
+    private function getNfirst(int $n = 10)
     {
-        $data = Json::DecodeFile(new Path($this->statsFile));
-        $data = $this->sortStructure($data);
-
-        return array_slice($data, 0, $n);
+        return array_slice(
+            $this->sortStructure(Json::DecodeFile(new Path($this->statsFile))),
+            0, $n
+        );
     }
 
     /** contruct a slack message */
     private function slackMessage()
     {
-        $mostUsed = $this->getNfirst();
-        $text = [];
-        foreach ($mostUsed as $emoji) {
-            $text[] = ":$emoji[0]: $emoji[1]";
-        }
-        $text[] = 'https://' .  $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-
-        return join(PHP_EOL, $text);
+        return join(PHP_EOL, array_reduce(
+            $this->getNfirst(),
+            function($text, $emoji) {
+                array_push($text, ":$emoji[1]: $emoji[0]");
+                return $text;
+            },
+            []
+        ));
     }
 
     private function sortStructure(array $data)
