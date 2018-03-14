4f7650cffd5b38e540c440e621a8b5b5c3cec2aa
jRimbault <jacques.rimbault@gmail.com>
2018-03-14 12:37:19 +0100

    doughnut chart

--- public/stats.js
+++ public/stats.js
@@ -1,5 +1,25 @@
 'use strict'
 
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex -= 1
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex]
+        array[currentIndex] = array[randomIndex]
+        array[randomIndex] = temporaryValue
+    }
+
+    return array
+}
+
 const request = {
     _objectToURI: object => {
         return Object.keys(object).map(key => {
@@ -31,7 +51,7 @@ const request = {
 const ctx = document.getElementById('chart-emojis').getContext('2d')
 
 const chartOptions = {
-    type: 'pie',
+    type: 'doughnut',
     data: {
         labels: [],
         datasets: [{
@@ -47,13 +67,17 @@ const chartOptions = {
         },
         tooltips: {
             displayColors: false
-        }
+        },
+        title: {
+            text: 'Top 20 Custom Emojis',
+            display: true
+        },
     }
 }
 
 request.post('/slack/list/emoji', [], xhr => {
-    let emojis = JSON.parse(xhr.responseText)
-    emojis = emojis.slice(0, 20)
+    let emojis = JSON.parse(xhr.responseText).slice(0, 20)
+    emojis = shuffle(emojis)
     let labels = emojis.reduce((labels, value) => {
         labels.push(value[1])
         return labels
@@ -70,7 +94,7 @@ request.post('/slack/list/emoji', [], xhr => {
 
     chartOptions.data.labels = labels
     chartOptions.data.datasets[0].data = data
-    chartOptions.data.datasets[0].backgroundColor = backgroundColor
+    chartOptions.data.datasets[0].backgroundColor = shuffle(backgroundColor)
 
     console.log(chartOptions)
 
