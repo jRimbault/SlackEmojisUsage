9a1e039dd131d72ac796eb59d9d346b30de0fefc
jRimbault <jacques.rimbault@gmail.com>
2018-04-18 17:30:30 +0200

    graph

--- public/stats.js
+++ public/stats.js
@@ -1,82 +1,65 @@
-'use strict'
+'use strict';
 
-function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex)
-        currentIndex -= 1
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex]
-        array[currentIndex] = array[randomIndex]
-        array[randomIndex] = temporaryValue
-    }
-
-    return array
-}
-
-const request = {
-    _objectToURI: object => {
-        return Object.keys(object).map(key => {
-            return encodeURIComponent(key) + '=' + encodeURIComponent(object[key])
-        }).join('&')
-    },
-    serialize: data => {
-        return typeof data == 'string' ? data : request._objectToURI(data)
-    },
-    get: (url, callback) => {
-        const xhr = new XMLHttpRequest()
-        xhr.open('GET', url)
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) callback(xhr)
+const data = new Promise(resolve => {
+    const r = fetch(
+        window.location.origin + '/slack/data/emoji', {
+            method: 'POST'
         }
-        xhr.send()
-    },
-    post: (url, data, callback) => {
-        const xhr = new XMLHttpRequest()
-        xhr.open('POST', url)
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) callback(xhr)
-        }
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-        xhr.send(request.serialize(data))
+    );
+    r.then(response => {
+        return resolve(response.json());
+    });
+    r.catch(error => {
+        console.log(error);
+    });
+});
+
+data.then(json => {
+    const top = 5;
+    const emojis = json.slice(0, top);
+    for (let i = 0; i < top; i += 1) {
+        chartOptions.data.datasets[i].label = emojis[i][0];
+        chartOptions.data.datasets[i].data = emojis[i][2];
+        chartOptions.data.datasets[i].fill = false;
     }
-}
+    for (let i = 0; i < emojis[0][2].length; i += 1) {
+        chartOptions.data.labels.push(i + 1);
+    }
+    const myChart = new Chart(ctx, chartOptions);
+});
+
 
 const ctx = document.getElementById('chart-emojis').getContext('2d')
 
 const chartOptions = {
-    type: 'doughnut',
+    type: 'line',
     data: {
         labels: [],
         datasets: [{
-            label: '# of Uses',
-            data: [],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)'
-            ],
+            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+            borderColor: ['rgba(255,99,132,1)'],
+            borderWidth: 1
+        }, {
+            backgroundColor: ['rgba(54, 162, 235, 0.2)', ],
+            borderColor: ['rgba(54, 162, 235, 1)'],
+            borderWidth: 1
+        }, {
+            backgroundColor: ['rgba(255, 206, 86, 0.2)'],
+            borderColor: ['rgba(255, 206, 86, 1)'],
+            borderWidth: 1
+        }, {
+            backgroundColor: ['rgba(75, 192, 192, 0.2)'],
+            borderColor: ['rgba(75, 192, 192, 1)'],
+            borderWidth: 1
+        }, {
+            backgroundColor: ['rgba(153, 102, 255, 0.2)'],
+            borderColor: ['rgba(153, 102, 255, 1)'],
             borderWidth: 1
         }]
     },
     options: {
         legend: {
-            display: false
+            display: true
         },
         tooltips: {
             displayColors: false
@@ -86,29 +69,4 @@ const chartOptions = {
             display: true
         },
     }
-}
-
-// https://github.com/chartjs/Chart.js/issues/815
-request.post('/slack/list/emoji', [], xhr => {
-    let emojis = JSON.parse(xhr.responseText).slice(0, 5)
-    let labels = emojis.reduce((labels, value) => {
-        labels.push(value[1])
-        return labels
-    }, [])
-
-    let data = emojis.reduce((data, value) => {
-        data.push(value[0])
-        return data
-    }, [])
-
-    let backgroundColor = palette('tol-rainbow', data.length).map(hex => {
-        return '#' + hex
-    })
-
-    chartOptions.data.labels = labels
-    chartOptions.data.datasets[0].data = data
-
-    console.log(chartOptions)
-
-    const myChart = new Chart(ctx, chartOptions)
-})
+};
