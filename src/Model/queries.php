b9a8e65e0859d02dd1741df1ff594900bdb0bf94
jRimbault <jacques.rimbault@gmail.com>
2018-04-18 13:21:24 +0200

    cosmetic

--- src/Model/queries.php
+++ src/Model/queries.php
@@ -1,33 +1,42 @@
 <?php
 
+// get all emojis' names
+$getAllNames =
+    'SELECT name from emoji';
+
+// get all, without count order sequence guarantee
+$getAllEmoji =
+    'SELECT
+        e.name as name,
+        e.url as url,
+        group_concat(c.count) as count
+    FROM
+        emoji AS e,
+        count AS c
+    WHERE e.id = c.id
+    GROUP BY e.name';
+
+// get data about a single emoji, count order sequence guarenteed
+$selectEmoji =
+    'SELECT
+        e.name as name,
+        e.url as url,
+        group_concat(count) as count
+    FROM emoji AS e, (
+        SELECT count
+        FROM count
+        WHERE id = (
+            SELECT id
+            FROM emoji
+            WHERE name = ?
+        )
+        ORDER BY date ASC
+    )
+    WHERE e.name = ?';
+
 
 return [
-    // get all names
-    'names' => 'SELECT name from emoji',
-    // get all
-    'general' => 'SELECT
-                      e.name as name,
-                      e.url as url,
-                      group_concat(c.count) as count
-                  FROM
-                      emoji AS e,
-                      count AS c
-                  WHERE e.id = c.id
-                  GROUP BY e.name',
-    // get data about a single emoji
-    'emoji' => 'SELECT
-                    e.name as name,
-                    e.url as url,
-                    group_concat(count) as count
-                FROM emoji AS e, (
-                    SELECT count
-                    FROM count
-                    WHERE id = (
-                        SELECT id
-                        FROM emoji
-                        WHERE name = ?
-                    )
-                    ORDER BY date ASC
-                )
-                WHERE e.name = ?',
+    'names' => $getAllNames,
+    'general' => $getAllEmoji,
+    'emoji' => $selectEmoji,
 ];
