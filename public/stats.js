ce743292c55868c4e9e9e2f12cb87c518a3e6402
jRimbault <jacques.rimbault@gmail.com>
2018-04-18 21:19:43 +0200

    cosmetic

--- public/stats.js
+++ public/stats.js
@@ -1,5 +1,44 @@
 'use strict';
 
+const ctx = document.getElementById('chart-emojis').getContext('2d');
+
+const chartOptions = {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: []
+    },
+    options: {
+        legend: {
+            display: true
+        },
+        tooltips: {
+            displayColors: false
+        },
+        title: {
+            text: 'Top 5 Custom Emojis',
+            display: true
+        },
+    }
+};
+
+const colors = [{
+    backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+    borderColor: ['rgba(255,99,132,1)']
+}, {
+    backgroundColor: ['rgba(54, 162, 235, 0.2)', ],
+    borderColor: ['rgba(54, 162, 235, 1)']
+}, {
+    backgroundColor: ['rgba(255, 206, 86, 0.2)'],
+    borderColor: ['rgba(255, 206, 86, 1)']
+}, {
+    backgroundColor: ['rgba(75, 192, 192, 0.2)'],
+    borderColor: ['rgba(75, 192, 192, 1)']
+}, {
+    backgroundColor: ['rgba(153, 102, 255, 0.2)'],
+    borderColor: ['rgba(153, 102, 255, 1)']
+}];
+
 const data = new Promise(resolve => {
     const r = fetch(
         window.location.origin + '/slack/data/emoji', {
@@ -18,55 +57,20 @@ data.then(json => {
     const top = 5;
     const emojis = json.slice(0, top);
     for (let i = 0; i < top; i += 1) {
-        chartOptions.data.datasets[i].label = emojis[i][0];
-        chartOptions.data.datasets[i].data = emojis[i][2];
-        chartOptions.data.datasets[i].fill = false;
+        chartOptions.data.datasets[i] = {};
+        Object.assign(
+            chartOptions.data.datasets[i],
+            {
+                label: emojis[i][0],
+                data: emojis[i][2],
+                fill: false,
+                borderWidth: 1
+            }
+        );
+        Object.assign(chartOptions.data.datasets[i], colors[i]);
     }
-    for (let i = 0; i < emojis[0][2].length; i += 1) {
-        chartOptions.data.labels.push(emojis[0][3][i].substr(0, 16));
-    }
+    // initialize empty labels
+    let size = emojis[0][2].length;
+    while (size--) chartOptions.data.labels[size] = '';
     const myChart = new Chart(ctx, chartOptions);
 });
-
-
-const ctx = document.getElementById('chart-emojis').getContext('2d')
-
-const chartOptions = {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
-            borderColor: ['rgba(255,99,132,1)'],
-            borderWidth: 1
-        }, {
-            backgroundColor: ['rgba(54, 162, 235, 0.2)', ],
-            borderColor: ['rgba(54, 162, 235, 1)'],
-            borderWidth: 1
-        }, {
-            backgroundColor: ['rgba(255, 206, 86, 0.2)'],
-            borderColor: ['rgba(255, 206, 86, 1)'],
-            borderWidth: 1
-        }, {
-            backgroundColor: ['rgba(75, 192, 192, 0.2)'],
-            borderColor: ['rgba(75, 192, 192, 1)'],
-            borderWidth: 1
-        }, {
-            backgroundColor: ['rgba(153, 102, 255, 0.2)'],
-            borderColor: ['rgba(153, 102, 255, 1)'],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        legend: {
-            display: true
-        },
-        tooltips: {
-            displayColors: false
-        },
-        title: {
-            text: 'Top 5 Custom Emojis',
-            display: true
-        },
-    }
-};
