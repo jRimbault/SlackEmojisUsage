2821440743858a5465318ede04806d64daf29330
jRimbault <jacques.rimbault@gmail.com>
2018-04-18 15:25:28 +0200

    cosmetic

--- var/index.php
+++ var/index.php
@@ -1,4 +1,6 @@
-<canvas id="chart-emojis" width="100" height="100"></canvas>
+<div style="width: 400px; height: 400px;">
+    <canvas id="chart-emojis" width="100" height="100"></canvas>
+</div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
 <script>
 'use strict';
@@ -17,13 +19,14 @@ const data = new Promise(resolve => {
 });
 
 data.then(json => {
-    const emojis = json.slice(0, 5);
-    for (let i = 0; i < 5; i += 1) {
+    const top = 5;
+    const emojis = json.slice(0, top);
+    for (let i = 0; i < top; i += 1) {
         chartOptions.data.datasets[i].label = emojis[i][0];
         chartOptions.data.datasets[i].data = emojis[i][2];
         chartOptions.data.datasets[i].fill = false;
     }
-    const myChart = new Chart(ctx, chartOptions)
+    const myChart = new Chart(ctx, chartOptions);
 });
 
 
@@ -34,59 +37,28 @@ const chartOptions = {
     data: {
         labels: [],
         datasets: [{
-            label: '# of Uses',
-            data: [],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)'
-            ],
+            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+            borderColor: ['rgba(255,99,132,1)'],
             borderWidth: 1
         },{
-            label: '# of Uses',
-            data: [],
-            backgroundColor: [
-                'rgba(54, 162, 235, 0.2)',
-            ],
-            borderColor: [
-                'rgba(54, 162, 235, 1)'
-            ],
+            backgroundColor: ['rgba(54, 162, 235, 0.2)',],
+            borderColor: ['rgba(54, 162, 235, 1)'],
             borderWidth: 1
         },{
-            label: '# of Uses',
-            data: [],
-            backgroundColor: [
-                'rgba(255, 206, 86, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 206, 86, 1)'
-            ],
+            backgroundColor: ['rgba(255, 206, 86, 0.2)'],
+            borderColor: ['rgba(255, 206, 86, 1)'],
             borderWidth: 1
         },{
-            label: '# of Uses',
-            data: [],
-            backgroundColor: [
-                'rgba(75, 192, 192, 0.2)'
-            ],
-            borderColor: [
-                'rgba(75, 192, 192, 1)'
-            ],
+            backgroundColor: ['rgba(75, 192, 192, 0.2)'],
+            borderColor: ['rgba(75, 192, 192, 1)'],
             borderWidth: 1
         },{
-            label: '# of Uses',
-            data: [],
-            backgroundColor: [
-                'rgba(153, 102, 255, 0.2)'
-            ],
-            borderColor: [
-                'rgba(153, 102, 255, 1)'
-            ],
+            backgroundColor: ['rgba(153, 102, 255, 0.2)'],
+            borderColor: ['rgba(153, 102, 255, 1)'],
             borderWidth: 1
         }]
     },
     options: {
-        fill: false,
         legend: {
             display: false
         },
