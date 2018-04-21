c1e5b87de2d7d8e69e80576f45d7ab00ee611abe
jRimbault <jacques.rimbault@gmail.com>
2018-04-21 16:32:10 +0200

    strings to nowdoc

--- src/Model/queries.php
+++ src/Model/queries.php
@@ -1,40 +1,41 @@
 <?php
 
 // get all emojis' names
-$getAllNames =
-    'SELECT name from emoji';
+$getAllNames = 'SELECT name from emoji';
 
 // get all, without count order sequence guarantee
-$getAllEmoji =
-    'SELECT
-        e.name AS name,
-        e.url AS url,
-        group_concat(c.count) AS count
-    FROM
-        emoji AS e,
-        count AS c
-    WHERE e.id = c.id
-    GROUP BY e.name';
+$getAllEmoji = <<<'SQL'
+SELECT
+    e.name AS name,
+    e.url AS url,
+    group_concat(c.count) AS count
+FROM
+    emoji AS e,
+    count AS c
+WHERE e.id = c.id
+GROUP BY e.name
+SQL;
 
 // get data about a single emoji, count order sequence guarenteed
-$selectEmoji =
-    'SELECT
-        e.name AS name,
-        e.url AS url,
-        group_concat(count) AS count,
-        group_concat(date) as date
-    FROM emoji AS e, (
-        SELECT count, date
-        FROM count
-        WHERE id = (
-            SELECT id
-            FROM emoji
-            WHERE name = ?
-        )
-        ORDER BY date ASC
-        LIMIT 168
+$selectEmoji = <<<'SQL'
+SELECT
+    e.name AS name,
+    e.url AS url,
+    group_concat(count) AS count,
+    group_concat(date) as date
+FROM emoji AS e, (
+    SELECT count, date
+    FROM count
+    WHERE id = (
+        SELECT id
+        FROM emoji
+        WHERE name = ?
     )
-    WHERE e.name = ?';
+    ORDER BY date ASC
+    LIMIT 168
+)
+WHERE e.name = ?
+SQL;
 
 
 return [
